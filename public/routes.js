d435fee8635fee5aca4dc4dda4c98b5aa522a5df
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2015-02-06 11:00:44 +0200

    Move to new route api, :lipstick: package.json

--- public/routes.js
+++ public/routes.js
@@ -1,11 +1,11 @@
-{
+export default {
   "component": "app/page.jsx!",
 
   "/post/:pid": "app/article.jsx!",
 
   "/category/:cid": {
     "component": "app/article-list.jsx!",
-    
+
     "/post/:pid": "app/article.jsx!"
   }
-}
+};
--- testdb.js
+++ testdb.js
@@ -1,3 +1,2 @@
 var getMiddleware = require('./database');
-console.log(getMiddleware);
 module.exports.middleware = getMiddleware('public/posts');
